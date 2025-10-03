7d7a62c761e4b52c5f8bc7ef24c8c9c6e383b2af
YinaYan <1143124497@qq.com>
2025-10-04 01:33:46 +0800

    设置main函数永久阻塞

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <csignal>
+#include <unistd.h>
 
-int main(){
-    std::cout << "Hello, World!" << std::endl;
+void signal_handler(int sig) {
+    std::cout << "Signal " << sig << " received" << std::endl;
+}
+
+int main() {
+    // 注册信号处理
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIGTERM, signal_handler);
+    std::signal(SIGHUP, signal_handler);
+
+    pause();
+
+    std::cout << "eye exiting..." << std::endl;
+    sleep(1);
+    std::cout << "eye exiting gracefully." << std::endl;
     return 0;
 }
