115ab51b4c6702bf8e45f5cf53be2b109b0ddad6
YinaYan <1143124497@qq.com>
2025-11-16 21:42:17 +0800

    添加日志模块初始化

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <csignal>
 #include <unistd.h>
+#include "logger.h"
 #include "video_process_hi.h"
 
 void signal_handler(int sig) {
@@ -14,9 +15,13 @@ int main() {
     std::signal(SIGTERM, signal_handler);
     std::signal(SIGHUP, signal_handler);
 
+    loggerSpace::Logger::instance().init();
+
     videoProcessHi::getInstance().init();
 
     pause();
+
+    loggerSpace::Logger::instance().fini();
 
     std::cout << "eye exiting..." << std::endl;
     sleep(1);
