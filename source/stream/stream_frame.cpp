190ffb55049a415ed4af154569c31e8626a397c5
YinaYan <1143124497@qq.com>
2026-04-07 20:20:04 +0800

    refactor: StreamFrame uses FrameData instead of ot_venc_stream

--- source/stream/stream_frame.cpp
+++ source/stream/stream_frame.cpp
@@ -1,40 +1,22 @@
 #include "stream_frame.h"
-#include <cstring>
+#include <algorithm>
 
-StreamFrame::StreamFrame(VencChannel chn, StreamType type, CodecType codec, const ot_venc_stream& src)
-    : channel_(chn), type_(type), codec_type_(codec) {
-
-    timestamp_ = (src.pack_cnt > 0) ? src.pack[0].pts : 0;
-
-    pack_array_.resize(src.pack_cnt);
-    pack_buffers_.reserve(src.pack_cnt);
-    stream_.pack_cnt = src.pack_cnt;
-    stream_.pack = pack_array_.data();
-
-    for (uint32_t i = 0; i < src.pack_cnt; ++i) {
-        const ot_venc_pack& src_pack = src.pack[i];
-
-        td_u32 offset = src_pack.offset;
-        td_u32 data_len = src_pack.len - offset;
-
-        std::vector<uint8_t> buf(data_len);
-        memcpy(buf.data(), src_pack.addr + offset, data_len);
-
-        pack_array_[i].addr = buf.data();
-        pack_array_[i].phys_addr = 0;
-        pack_array_[i].len = data_len;
-        pack_array_[i].pts = src_pack.pts;
-        pack_array_[i].is_frame_end = src_pack.is_frame_end;
-        pack_array_[i].data_type = src_pack.data_type;
-        pack_array_[i].offset = 0;
-        pack_array_[i].stuff03_disable = src_pack.stuff03_disable;
-        pack_array_[i].layer_id = src_pack.layer_id;
-        pack_array_[i].is_lu_begin = src_pack.is_lu_begin;
-        pack_array_[i].data_num = src_pack.data_num;
-
-        pack_buffers_.push_back(std::move(buf));
-    }
+StreamFrame::StreamFrame(VencChannel chn, StreamType type, CodecType codec, const FrameData& data)
+    : channel_(chn), type_(type), codec_type_(codec), frame_data_(data) {
+    timestamp_ = 0;  // FrameData 中无 pts 字段，暂设为0
 }
 
 StreamFrame::~StreamFrame() {
+}
+
+bool StreamFrame::is_idr() const {
+    if (codec_type_ == CodecType::MJPEG) {
+        return true;
+    }
+    for (uint32_t i = 0; i < frame_data_.pack_count; ++i) {
+        if (frame_data_.packs[i].nalu_type == NaluType::IDR_SLICE) {
+            return true;
+        }
+    }
+    return false;
 }
--- source/stream/stream_frame.h
+++ source/stream/stream_frame.h
@@ -2,36 +2,30 @@
 #define _STREAM_FRAME_H_
 
 #include "stream_common.h"
-#include <ot_common_venc.h>
+#include "common_types.h"
 #include <memory>
-#include <vector>
-
-class StreamFrame;
-using StreamFramePtr = std::shared_ptr<StreamFrame>;
 
 class StreamFrame {
 public:
-    StreamFrame(VencChannel chn, StreamType type, CodecType codec, const ot_venc_stream& src);
+    StreamFrame(VencChannel chn, StreamType type, CodecType codec, const FrameData& data);
     ~StreamFrame();
-
     StreamFrame(const StreamFrame&) = delete;
     StreamFrame& operator=(const StreamFrame&) = delete;
-
     VencChannel channel() const { return channel_; }
     StreamType type() const { return type_; }
     CodecType codec_type() const { return codec_type_; }
     uint64_t timestamp() const { return timestamp_; }
-    const ot_venc_stream& data() const { return stream_; }
+    bool is_idr() const;
+    const FrameData& data() const { return frame_data_; }
 
 private:
     VencChannel channel_;
-    ot_venc_stream stream_;
-    uint64_t timestamp_;
     StreamType type_;
     CodecType codec_type_;
-
-    std::vector<ot_venc_pack> pack_array_;
-    std::vector<std::vector<uint8_t>> pack_buffers_;
+    uint64_t timestamp_;
+    FrameData frame_data_;
 };
+
+using StreamFramePtr = std::shared_ptr<StreamFrame>;
 
 #endif
